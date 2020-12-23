356894ec6e937539150a0518b68b307ee878b3f8
Александр Райш <leidenn2509@gmail.com>
2020-12-23 17:11:14 +0700

    Simple test

--- src/main/kotlin/ru/nsu/fit/conveyor/main.kt
+++ src/main/kotlin/ru/nsu/fit/conveyor/main.kt
@@ -5,6 +5,7 @@ import ru.nsu.fit.conveyor.baseNode.DataQueue
 import ru.nsu.fit.conveyor.baseNode.Flow
 import ru.nsu.fit.conveyor.baseNode.Node
 
+fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
 
 fun main() = runBlocking {
 //    val a = DataQueue()
@@ -17,26 +18,79 @@ fun main() = runBlocking {
 //    TODO()
 
     val powNode = Node("simple pow").apply {
-        addInput(0, Int::class.java)
-        addOutput(0, Int::class.java)
+        addInput(0, Double::class.java)
+        addOutput(0, Double::class.java)
         body = { map ->
             @Suppress("UNCHECKED_CAST")
-            val a = map.getValue(0) as List<Int>
-            mapOf(0 to a.map { it*it })
+            val a = map.getValue(0) as List<Double>
+            mapOf(0 to a.map { it * it })
+        }
+    }
+
+    val dividerNode = Node("2 to 3").apply {
+        addInput(0, Double::class.java)
+        addOutput(0, Double::class.java)
+        addOutput(1, Double::class.java)
+        body = { map ->
+            val a = map.getValue(0).map { (it as Double) / 5.0 }
+            mapOf(
+                0 to a.map { 2.0 * it },
+                1 to a.map { 3.0 * it }
+            )
+        }
+    }
+
+    val subNode = Node("-3").apply {
+        addInput(0, Double::class.java)
+        addOutput(0, Double::class.java)
+        body = { map ->
+            val a = map.getValue(0).map { (it as Double) - 3.0 }
+            mapOf(
+                0 to a
+            )
+        }
+    }
+
+    val sumNode = Node("+").apply {
+        addInput(0, Double::class.java)
+        addInput(1, Double::class.java)
+        addOutput(0, Double::class.java)
+        body = { map ->
+            val a = map.getValue(0).map { it as Double }
+            val b = map.getValue(1).map { it as Double }
+            mapOf(
+                0 to a.zip(b).map { it.first + it.second }
+            )
         }
     }
 
 //    println(powNode.run(mapOf(0 to listOf(8))))
 
-    val flow = Flow("pow twice").apply {
-        addInput(0, Int::class.java)
-        addOutput(0, Int::class.java)
+//    val flow = Flow("pow twice").apply {
+//        addInput(0, Double::class.java)
+//        addOutput(0, Double::class.java)
+//
+//        addNode("pow1", powNode).flowInput(0, 0)
+//        addNode("pow2", powNode)
+//        addNode("pow3", powNode).flowOutput(0, 0)
+//        connect("pow1", 0, "pow2", 0)
+//        connect("pow2", 0, "pow3", 0)
+//    }
+//    println(flow.run(mapOf(0 to listOf(1, 2, 3))))
 
-        addNode("pow1", powNode).flowInput(0, 0)
-        addNode("pow2", powNode)
-        addNode("pow3", powNode).flowOutput(0, 0)
-        connect("pow1", 0, "pow2", 0)
-        connect("pow2", 0, "pow3", 0)
+    val complexFlow = Flow("hard math").apply {
+        addInput(0, Double::class.java)
+        addOutput(0, Double::class.java)
+
+        addNode("divider", dividerNode).flowInput(0, 0)
+        addNode("sub", subNode)
+        addNode("pow", powNode)
+        addNode("sum", sumNode).flowOutput(0, 0)
+
+        connect("divider", 0, "sub", 0)
+        connect("divider", 1, "pow", 0)
+        connect("sub", 0, "sum", 0)
+        connect("pow", 0, "sum", 1)
     }
-    println(flow.run(mapOf(0 to listOf(1, 2, 3))))
+    println(complexFlow.run(mapOf(0 to listOf(5.0, 10.0, 3.0))))
 }
