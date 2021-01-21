0bd838edc415b12c9adb5eae44d05123f3ffb384
Александр Райш <leidenn2509@gmail.com>
2021-01-22 02:27:48 +0700

    One instance heh

--- src/main/kotlin/ru/nsu/fit/conveyor/node/BaseNode.kt
+++ src/main/kotlin/ru/nsu/fit/conveyor/node/BaseNode.kt
@@ -25,24 +25,32 @@ data class NodeOutput<T: Any>(
     var connectedTo: BaseNode? = null
 )
 
+
 @ExperimentalCoroutinesApi
 abstract class BaseNode(
     val description: String
 ) {
-    // FIXME replace map with list
-    val outputs = mutableMapOf<Int, NodeOutput<Any>>()
-    val inputs = mutableMapOf<Int, NodeInput<Any>>()
+    val contexts = mutableMapOf<Any?, Context>()
 
-    suspend fun <T: Any> sendArg(id: Int, data: T) {
-        inputs[id]?.let {
+    // FIXME replace map with list
+
+    abstract inner class Context {
+        val outputs = mutableMapOf<Int, NodeOutput<Any>>()
+        val inputs = mutableMapOf<Int, NodeInput<Any>>()
+    }
+
+    abstract fun initContext(withContext: Any?)
+
+    suspend fun <T: Any> sendArg(id: Int, data: T, withContext: Any? = null) {
+        contexts[withContext]!!.inputs[id]?.let {
             if (data::class != it.type)
                 error("[$description] Data type not match with input type: ${data::class} vs ${it.type}")
             it.channel.send(data)
         } ?: error("[$description] No input with id=$id")
     }
 
-    suspend fun <T: Any> receiveArg(id: Int): T {
-        return outputs[id]?.let {
+    suspend fun <T: Any> receiveArg(id: Int, withContext: Any? = null): T {
+        return contexts[withContext]!!.outputs[id]?.let {
             val data = it.channel.receive()
             @Suppress("UNCHECKED_CAST")
             data as? T ?: error("[$description] Data type not match with output type: ${data::class} vs ${it.type}")
@@ -50,10 +58,10 @@ abstract class BaseNode(
     }
 
     // Может лучше запретить коннекты вне потока?
-    fun connectOutputTo(outputId: Int, to: BaseNode, inputId: Int) {
+    fun connectOutputTo(outputId: Int, to: BaseNode, inputId: Int, withContext: Any? = null) {
         // TODO Check for channel not null
-        outputs[outputId]?.let {
-            val nodeInput = to.inputs[inputId]!!
+        contexts[withContext]!!.outputs[outputId]?.let {
+            val nodeInput = to.contexts[withContext]!!.inputs[inputId]!!
             if (it.type != nodeInput.type)
                 error("[$description] Type of input and output don't match: ${it.type} vs ${nodeInput.type}")
             it.channel = nodeInput.channel
@@ -62,34 +70,34 @@ abstract class BaseNode(
 
     }
 
-    protected open val isReady
-        get() = inputs.all { (_, input) ->
+    open fun isReady(withContext: Any?) =
+        contexts[withContext]!!.inputs.all { (_, input) ->
             !input.channel.isEmpty
         }
 
     var isRunning: Boolean = false
         protected set
 
-    open suspend fun tryRun(coroutineScope: CoroutineScope): Job = coroutineScope.launch {
-        log("Try run")
-        while (isReady) {
-            log("Ready and run")
+    open suspend fun tryRun(coroutineScope: CoroutineScope, withContext: Any? = null): Job = coroutineScope.launch {
+        log("Try run", contexts[withContext]!!)
+        while (isReady(withContext)) {
+            log("Ready and run", contexts[withContext]!!)
             isRunning = true
-            body()
-            tryRunOutputs(coroutineScope)
+            body(contexts[withContext]!!)
+            tryRunOutputs(coroutineScope, withContext)
         }
         isRunning = false
     }
 
-    private suspend fun tryRunOutputs(coroutineScope: CoroutineScope) {
-        log("Try run outputs")
-        outputs.forEach { (_, nodeOutput) ->
-            log("Try run ${nodeOutput.connectedTo}")
-            nodeOutput.connectedTo?.tryRun(coroutineScope)
+    private suspend fun tryRunOutputs(coroutineScope: CoroutineScope, withContext: Any?) {
+        log("Try run outputs", contexts[withContext]!!)
+        contexts[withContext]!!.outputs.forEach { (_, nodeOutput) ->
+            log("Try run ${nodeOutput.connectedTo}", contexts[withContext]!!)
+            nodeOutput.connectedTo?.tryRun(coroutineScope, withContext)
         }
     }
 
-    abstract suspend fun body()
+    abstract suspend fun body(context: Context)
 
-    protected fun log(msg: String) = println("[${Thread.currentThread().name}][${this.description}] $msg")
+    protected fun log(msg: String, context: Context) = println("[${Thread.currentThread().name}][${this.description}] $msg")
 }
