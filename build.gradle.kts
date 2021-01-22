24a71a17d71cbd174a6fd8b4bf7b58ee89efb6f7
rbdv <i.budaeva@g.nsu.ru>
2021-01-23 04:20:29 +0700

    better-parser lib added to env

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { setUrl("https://dl.bintray.com/hotkeytlt/maven") }
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+
+    implementation ("com.github.h0tk3y.betterParse:better-parse:0.4.0")
 
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
